62971ba9c4253ecd1b9f4d51ff5915864f8b7499
Martin Prout <monkstone@somewhere.org>
2011-07-04 19:04:29 +0000

    Added new look up table with ca. 0.25 degree precision

---
+++ LSystem/src/lsystem/util/LUT2.java
@@ -0,0 +1,96 @@
+/** 
+ * Copyright (c) 2011 Martin Prout
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * http://creativecommons.org/licenses/LGPL/2.1/
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+/**
+ * A restricted lookup table for fast sine & cosine computations. The table 
+ * currently has a fixed precision of 0.25 degrees. Thus might be adequate for 
+ * many purposes. There will be errors. Note the reduced lookup up table, 
+ * is restricted to the first quadrant of sine. Conditional rules are used to 
+ * map to other quadrants and cos. Based on ideas from:-
+ * http://en.wikipedia.org/wiki/Lookup_table
+ * One annoyance of java is the behaviour of % wrt negative values cf python for 
+ * example. A kludge is required to return the complement of 360, which would 
+ * not otherwise be required.
+ * Double precision will be impossible unless input values are doubles with no 
+ * decimal part, so we cast to float for lookup table to save a bit of space
+ */
+package lsystem.util;
+
+
+public class LUT2 {
+
+    /**
+     * LUT for sine values 0.25 degree increments of first quadrant
+     */
+    public static float[] sinLUT = new float[361];
+
+
+    /**
+     * Initialise sin table with values (first quadrant only)
+     * Save a bit of space by storing as float
+     */
+    public static void initialize() {
+        for (int i = 0; i <= 360; i++) {
+            sinLUT[i] = (float)Math.sin(Math.toRadians(i/4));
+        }
+    }
+
+    /**
+     * Look up sine for the passed angle in degrees.
+     * 
+     * @param thet degree int
+     * @return sine value for theta
+     */
+    public static float sin(float thet) {
+        while (thet < 0) {
+            thet += 360; // Needed because negative modulus plays badly in java
+        }
+        int theta = Math.round(thet * 4) % 1440;
+        int y = theta % 360;
+        float result = (theta < 360) ?  sinLUT[y] : (theta < 720)
+                ? sinLUT[(360 - y)] : (theta < 1080)
+                ? -sinLUT[y] : -sinLUT[(360 - y)];
+        return result;
+    }
+    
+   
+
+    /**
+     * Look up cos for the passed angle in degrees, fixed precision, no better
+     * than 0.25 degree with some scope for errors.
+     * 
+     * @param thet degree float
+     * @return sine value for theta
+     */
+    public static float cos(float thet) {
+        while (thet < 0) {
+            thet += 360; // Needed because negative modulus plays badly in java
+        }
+        int theta = Math.round(thet * 4) % 1440;
+        int y = theta % 360;
+        float result = (theta < 360) ? sinLUT[360 - y] : (theta < 720)
+                ? -sinLUT[y] : (theta < 1080)
+                ? -sinLUT[360 - y] :  sinLUT[y];
+        return result;
+    }
+      
+}
+
---
+++ LSystem/test/lsystem/util/LUT2Test.java
@@ -0,0 +1,108 @@
+package lsystem.util;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Martin Prout <martin_p@lineone.net>
+ */
+public class LUT2Test {
+    
+    public LUT2Test() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+        LUT2.initialize();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
+    /**
+     * Test of sin method, of class LUT2.
+     */
+    @Test
+    public void testMinusSin() {
+        System.out.println("minus sin");
+        float thet = -37.5F;
+        float expResult = (float)Math.sin(Math.toRadians(thet));
+        float result = LUT2.sin(thet);
+        assertEquals(expResult, result, 0.01F);
+    }
+
+    /**
+     * Test of cos method, of class LUT2.
+     */
+    @Test
+    public void testMinusCos() {
+        System.out.println("minus cos");
+        float thet = -37.5F;
+        float expResult = (float)Math.cos(Math.toRadians(thet));
+        float result = LUT2.cos(thet);
+        assertEquals(expResult, result, 0.01F);
+    }
+    
+        /**
+     * Test of sin method, of class LUT2.
+     */
+    @Test
+    public void testSin() {
+        System.out.println("sin");
+        float thet = 90.5F;
+        float expResult = (float)Math.sin(Math.toRadians(thet));
+        float result = LUT2.sin(thet);
+        assertEquals(expResult, result, 0.01F);
+    }
+
+    /**
+     * Test of cos method, of class LUT2.
+     */
+    @Test
+    public void testCos() {
+        System.out.println("cos");
+        float thet = 90.5F;
+        float expResult = (float)Math.cos(Math.toRadians(thet));
+        float result = LUT2.cos(thet);
+        assertEquals(expResult, result, 0.01F);
+    }
+    
+            /**
+     * Test of sin method, of class LUT2.
+     */
+    @Test
+    public void testLargeSin() {
+        System.out.println("large sin");
+        float thet = 2000.5F;
+        float expResult = (float)Math.sin(Math.toRadians(thet));
+        float result = LUT2.sin(thet);
+        assertEquals(expResult, result, 0.01F);
+    }
+
+    /**
+     * Test of cos method, of class LUT2.
+     */
+    @Test
+    public void testLargeCos() {
+        System.out.println("large cos");
+        float thet = 2000.5F;
+        float expResult = (float)Math.cos(Math.toRadians(thet));
+        float result = LUT2.cos(thet);
+        assertEquals(expResult, result, 0.01F);
+    }
+}
