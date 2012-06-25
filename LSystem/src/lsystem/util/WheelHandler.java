416dd44c7885a9e2789c79ab01c2ba2c84a1ed14
Martin Prout <monkstone@somewhere.org>
2012-06-25 14:52:12 +0000

    More documentation

--- LSystem/src/lsystem/util/WheelHandler.java
+++ LSystem/src/lsystem/util/WheelHandler.java
@@ -1,12 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * The purpose of this library is to allow users to explore lystems using
+ * processing sketches Copyright (C) 2012 Martin Prout This library is free
+ * software; you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * Obtain a copy of the license at http://www.gnu.org/licenses/lgpl-2.1.html
  */
 package lsystem.util;
 
 /**
- *
- * @author tux
+ * @author Martin Prout 
+ * from a borrowed pattern seen in Jonathan Feibergs Peasycam
+ * when I was struggling with non functioning browser applet, 
+ * probably superfluous here.
  */
 public interface WheelHandler { 
     /**
