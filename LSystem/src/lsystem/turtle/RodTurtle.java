3329686bbe8211cccf44c7876106456b64080ef4
Martin Prout <monkstone@somewhere.org>
2012-08-09 17:38:37 +0000

    Create a new drawRod function to allow creation of truncated cone (mainly for 3D plants).

--- LSystem/src/lsystem/turtle/RodTurtle.java
+++ LSystem/src/lsystem/turtle/RodTurtle.java
@@ -47,13 +47,13 @@ public class RodTurtle implements Turtle3D {
     public void draw(float distance) {
         drawRod(distance);
     }
-    
 
     /**
-     * 
+     *
      * @param distance
      * @param depth
      */
+    @Override
     public void draw(float distance, int depth) {
         drawRod(distance, depth);
     }
@@ -106,39 +106,57 @@ public class RodTurtle implements Turtle3D {
     }
 
     /**
+     * Wrapper method
      * Draw a smooth cylinder capped at one end with a sphere uses a look up
-     * table for sin and cos (NB: degree not radians)
-     *
+     * table for sin and cos (NB: degree not radians), arbitrary set ratio of
+     * distance & cylinder radius & detail/level
      * @param distance the length of the cylinder
      */
     public void drawRod(float distance) {
-        drawRod(distance, 3); // arbitrary level of 3 for detail
+        drawRod(distance, distance / 7, distance / 7, 3);
     }
 
     /**
+     * Wrapper method
      * Draw a smooth cylinder capped at one end with a sphere uses a look up
      * table for sin and cos (NB: degree not radians)
      *
      * @param distance the length of the cylinder
-     * @param level detail ie inverse of level 
+     * @param level detail ie inverse of level
      */
+    
     public void drawRod(float distance, int level) {
+        drawRod(distance, distance / 7, distance / 7, level);
+    }
+    
+   /**
+    * Real Method
+    * Draw a smooth cone section with radii r1, r2, perpendicular distance 
+    * level is inverse of detail, capped with a sphere
+    * @param distance
+    * @param r1
+    * @param r2
+    * @param level 
+    */
+
+   
+    @Override
+        public void drawRod(float distance, float r1, float r2, int level) {
         int sides = detail[level]; // ensure 360 % sides is zero
-        float r = distance / 7;
         int angle = 0;
         int angleIncrement = 360 / sides;
-        endCap(distance / 7, sides);
+       // endCap(r1, sides);
         parent.translate(0, 0, distance / 2);
         parent.beginShape(PApplet.QUAD_STRIP);
         for (int i = 0; i <= sides; i++) {
             parent.normal(LUT.cos(angle), LUT.sin(angle), 0);
-            parent.vertex(r * LUT.cos(angle), r * LUT.sin(angle), distance / 2);
-            parent.vertex(r * LUT.cos(angle), r * LUT.sin(angle), -distance / 2);
+            parent.vertex(r1 * LUT.cos(angle), r1 * LUT.sin(angle), distance / 2);
+            parent.vertex(r2 * LUT.cos(angle), r2 * LUT.sin(angle), -distance / 2);
             angle += angleIncrement;
         }
-        parent.endShape();
+        parent.endShape();        
         parent.translate(0, 0, distance / 2);
-
+        endCap(r1, sides);
     }
 
     /**
