9baceebad6ec52d25ed8daee34f1c0d72c3d3e0e
Martin Prout <monkstone@somewhere.org>
2014-01-24 16:19:31 +0000

    Update documentation

--- LSystem/src/lsystem/turtle/Turtle3D.java
+++ LSystem/src/lsystem/turtle/Turtle3D.java
@@ -1,5 +1,5 @@
     /* 
- * Copyright (c) 2011/13 Martin Prout
+ * Copyright (c) 2011-14 Martin Prout
  * 
  * This demo & library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
